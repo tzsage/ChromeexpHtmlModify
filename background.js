4cc2011385e1eaaa0dbabcfcaf0779e3c39fa935
tz <tzsage@163.com>
2016-11-09 09:57:42 +0800

    add

---
+++ background.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+
+$(function () {
+    $("img").each(function () {
+       $(this).attr("src", "https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1599508696,1881405948&fm=80&w=179&h=119&img.JPEG");
+    })
+})
